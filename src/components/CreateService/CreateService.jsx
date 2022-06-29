916877216d1ee618b939dc606b8444db1fef0640
Carlos Araújo <carlosaaraujopc@gmail.com>
2022-06-29 17:52:52 -0300

    Salvando todas informações dos inputs no state

--- src/components/CreateService/CreateService.jsx
+++ src/components/CreateService/CreateService.jsx
@@ -1,25 +1,91 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class CreateService extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Cadastre o seu serviço</h2>
-        <input type={"text"} placeholder="Título"/>
-        <input type={"text"} placeholder="Descrição"/>
-        <input type={"number"} placeholder="Preço"/>
+	state = {
+		title: "",
+		description: "",
+		price: "",
+		selectedPayments: [],
+		date: "",
+	};
 
-        <select multiple>
-          <option>Cartão de crédito</option>
-          <option>Cartão de débito</option>
-          <option>PayPal</option>
-          <option>Boleto</option>
-          <option>Pix</option>
-        </select>
+	onChangeInputTitle = (e) => {
+		this.setState({ title: e.target.value });
+	};
 
-        <input type="date" />
-        <button>Cadastrar Serviço</button>
-      </div>
-    )
-  }
+	onChangeInputDescription = (e) => {
+		this.setState({ description: e.target.value });
+	};
+
+	onChangeInputPrice = (e) => {
+		this.setState({ price: e.target.value });
+	};
+
+	onChangeSelectPayments = (e) => {
+		this.setState({
+			selectedPayments: Array.from(
+				e.target.selectedOptions,
+				(item) => item.value
+			),
+		});
+	};
+
+	onChangeInputDate = (e) => {
+		this.setState({ date: e.target.value });
+	};
+
+	createService = () => {
+		alert("Serviço criado com sucesso!");
+	};
+
+	render() {
+		return (
+			<div>
+				<h2>Cadastre o seu serviço</h2>
+				<input
+					type={"text"}
+					placeholder="Título do serviço"
+					value={this.state.title}
+					onChange={this.onChangeInputTitle}
+				/>
+
+				<input
+					type={"text"}
+					placeholder="Descrição do serviço"
+					value={this.state.description}
+					onChange={this.onChangeInputDescription}
+				/>
+
+				<input
+					type={"number"}
+					placeholder="Preço"
+					value={this.state.price}
+					onChange={this.onChangeInputPrice}
+				/>
+
+				<select
+					multiple
+					value={this.state.selectedPayments}
+					onChange={this.onChangeSelectPayments}
+				>
+					<option>Cartão de crédito</option>
+					<option>Cartão de débito</option>
+					<option>PayPal</option>
+					<option>Boleto</option>
+					<option>Pix</option>
+				</select>
+
+				<input
+					type="date"
+					placeholder="Data do serviço"
+					value={this.state.date}
+					onChange={this.onChangeInputDate}
+				/>
+
+				<button onClick={this.createService}>
+					Cadastrar Serviço
+				</button>
+			</div>
+		);
+	}
 }
